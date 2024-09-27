7b91b5138a36b5415f165760312bd18586c6a6bf
DaleNNN <oysteindale@live.com>
2024-09-27 08:32:36 +0200

    Added calibration function May move to separate calibrate-file

--- src/main.cpp
+++ src/main.cpp
@@ -39,6 +39,38 @@ void setup() {
     pinMode(motorREnable, OUTPUT);
     pinMode(motorRPos, OUTPUT);
     pinMode(motorRNeg, OUTPUT);
+
+    delay(500);
+    pinMode(LED_BUILTIN, OUTPUT);
+    digitalWrite(LED_BUILTIN, HIGH); // turn on Arduino's LED to indicate we are in calibration mode
+
+    // 2.5 ms RC read timeout (default) * 10 reads per calibrate() call
+    // = ~25 ms per calibrate() call.
+    // Call calibrate() 400 times to make calibration take about 10 seconds.
+    for (uint16_t i = 0; i < 400; i++)
+    {
+        sensors.calibrate();
+    }
+    digitalWrite(LED_BUILTIN, LOW); // turn off Arduino's LED to indicate we are through with calibration
+
+    // print the calibration minimum values measured when emitters were on
+    Serial.begin(9600);
+    for (uint8_t i = 0; i < SensorCount; i++)
+    {
+        Serial.print(sensors.calibrationOn.minimum[i]);
+        Serial.print(' ');
+    }
+    Serial.println();
+
+    // print the calibration maximum values measured when emitters were on
+    for (uint8_t i = 0; i < SensorCount; i++)
+    {
+        Serial.print(sensors.calibrationOn.maximum[i]);
+        Serial.print(' ');
+    }
+    Serial.println();
+    Serial.println();
+    delay(1000);
 }
 
 //Declaration of functions to be implemented later
