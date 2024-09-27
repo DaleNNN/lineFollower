1f5bc76a94c493c5addac6c23b01a8634993395a
DaleNNN <oysteindale@live.com>
2024-09-27 10:37:41 +0200

    fixed bugs

--- src/main.cpp
+++ src/main.cpp
@@ -18,7 +18,7 @@ constexpr int motorREnable = 11; //Enabler and speed(PWM) for right motor
 int leftSpeed = 0;
 int rightSpeed = 0;
 
-int threshold = 800; //Threshold for reflectance
+int threshold = 1000; //Threshold for reflectance
 int readingL = 0;
 int readingR = 0; //Declaring reading variables for all sensors
 int readingML = 0;
@@ -47,7 +47,8 @@ void setup() {
     // 2.5 ms RC read timeout (default) * 10 reads per calibrate() call
     // = ~25 ms per calibrate() call.
     // Call calibrate() 400 times to make calibration take about 10 seconds.
-    for (uint16_t i = 0; i < 400; i++)
+    Serial.print("Calibration has begun");
+    for (uint16_t i = 0; i < 50; i++)
     {
         sensors.calibrate();
     }
@@ -71,6 +72,10 @@ void setup() {
     Serial.println();
     Serial.println();
     delay(1000);
+
+    //Set threshold to value from calibration earlier.
+    //threshold = sensors.readLineBlack(sensorValues);
+    Serial.print("Sensors have been calibrated to value of " + threshold);
 }
 
 //Declaration of functions to be implemented later
@@ -81,63 +86,64 @@ void serialLog();
 void loop() {
     //read sensor readings
     sensors.read(sensorValues);
-    //Set threshold to value from calibration earlier.
-    threshold = sensors.readLineBlack(sensorValues);
-    Serial.println("Sensors have been calibrated to value of " + threshold);
+    threshold = 1000;
+
 
     //Save each reading in own variable
-    readingL = sensorValues[2];
-    readingR = sensorValues[3];
-    readingML = sensorValues[0];
-    readingMR = sensorValues[1];
+    readingL = sensorValues[3];
+    readingR = sensorValues[2];
+    readingML = sensorValues[1];
+    readingMR = sensorValues[0];
 
 
     // *FOR DEBUG*
     // print the sensor values as numbers from 0 to 2500, where 0 means maximum
     // reflectance and 2500 means minimum reflectance
-    /*for (uint8_t i = 0; i < SensorCount; i++)
+    for (uint8_t i = 3; i < SensorCount; i++)
     {
-        Serial.print(sensorValues[i]);
+        Serial.print(String(readingL) + " " + String(readingML) + " " + String(readingMR) + " " + String(readingR));
         Serial.print('\t');
     }
-    Serial.println();*/
+    Serial.println();
 
 
-    serialLog();
+    //serialLog();
 
 
-    if ((readingL > threshold) && (readingR > threshold)
-        && (readingML > threshold) && (readingMR > threshold)) {
+    if ((readingL < threshold) && (readingR < threshold)
+        && (readingML < threshold) && (readingMR < threshold)) {
         //If no line is detected
         stopMotors();
-    } else if ((readingL > threshold) && (readingML > threshold)
-               && (readingMR < threshold) && (readingR < threshold)) {
+    } else if ((readingL < threshold) && (readingML < threshold)
+               && (readingMR < threshold) && (readingR > threshold)) {
         //If right and midright detects line
         leftSpeed = 255;
-        rightSpeed = 100;
-    } else if ((readingL < threshold) && (readingML < threshold)
-               && (readingMR > threshold) && (readingR > threshold)) {
-        //If left and midleft detects line
-        leftSpeed = 100;
-        rightSpeed = 255;
-    } else if ((readingL > threshold) && (readingML < threshold)
-               && (readingMR > threshold) && (readingR > threshold)) {
-        //If only midleft detects line
-        leftSpeed = 175;
-        rightSpeed = 255;
+        rightSpeed = 80;
     } else if ((readingL > threshold) && (readingML > threshold)
-               && (readingMR < threshold) && (readingR > threshold)) {
+               && (readingMR < threshold) && (readingR < threshold)) {
+        //If left and midleft detects line
+        leftSpeed = 80;
+        rightSpeed = 255;
+    } else if ((readingL < threshold) && (readingML > threshold)
+               && (readingMR < threshold) && (readingR < threshold)) {
+        //If only midleft detects line
+        leftSpeed = 160;
+        rightSpeed = 255;
+    } else if ((readingL < threshold) && (readingML < threshold)
+               && (readingMR > threshold) && (readingR < threshold)) {
         //If only midright detects line
         leftSpeed = 255;
-        rightSpeed = 175;
-    } else if ((readingL < threshold) && (readingML > threshold)) {
+        rightSpeed = 160;
+    } else if ((readingL > threshold) && (readingML < threshold)
+            && (readingMR < threshold) && (readingR < threshold)) {
         //If only left detects line
-        leftSpeed = 50;
+        leftSpeed = 10;
         rightSpeed = 255;
-    } else if ((readingR < threshold) && readingMR > threshold) {
+    } else if ((readingR > threshold) && (readingL < threshold)
+            && (readingMR < threshold) && (readingML < threshold)) {
         //If only right detects line
         leftSpeed = 255;
-        rightSpeed = 50;
+        rightSpeed = 10;
     } else {
         //If none of the above, go forward
         leftSpeed = 255;
