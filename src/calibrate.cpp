52e061ae08e3dc204cd2721cbecf2d4d653becad
DaleNNN <oysteindale@live.com>
2024-09-27 10:57:37 +0200

    Moved calibration to calibrate.cpp

---
+++ src/calibrate.cpp
@@ -0,0 +1,41 @@
+#include <Arduino.h>
+#include <QTRSensors.h>
+#include "main.cpp"
+
+void runCalibrate() {
+    pinMode(LED_BUILTIN, OUTPUT);
+    digitalWrite(LED_BUILTIN, HIGH); // turn on Arduino's LED to indicate we are in calibration mode
+
+    // 2.5 ms RC read timeout (default) * 10 reads per calibrate() call
+    // = ~25 ms per calibrate() call.
+    // Call calibrate() 400 times to make calibration take about 10 seconds.
+    Serial.print("Calibration has begun");
+    for (uint16_t i = 0; i < 50; i++)
+    {
+        sensors.calibrate();
+    }
+    digitalWrite(LED_BUILTIN, LOW); // turn off Arduino's LED to indicate we are through with calibration
+
+    // print the calibration minimum values measured when emitters were on
+    Serial.begin(9600);
+    for (uint8_t i = 0; i < SensorCount; i++)
+    {
+        Serial.print(sensors.calibrationOn.minimum[i]);
+        Serial.print(' ');
+    }
+    Serial.println();
+
+    // print the calibration maximum values measured when emitters were on
+    for (uint8_t i = 0; i < SensorCount; i++)
+    {
+        Serial.print(sensors.calibrationOn.maximum[i]);
+        Serial.print(' ');
+    }
+    Serial.println();
+    Serial.println();
+    delay(1000);
+
+    //Set threshold to value from calibration earlier.
+    //threshold = sensors.readLineBlack(sensorValues);
+    Serial.print("Sensors have been calibrated to value of " + threshold);
+}
